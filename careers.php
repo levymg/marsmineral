d8a2c02a3f1b2bb999310faeed575f05044ed36d
Levy-mini <levy-mini@Levy-minis-Mac-mini.local>
2015-03-12 14:20:32 -0400

    More copy changes

--- careers.php
+++ careers.php
@@ -6,7 +6,7 @@
     <div class="content">
     	<div class="col-left">
         	
-            <h1>Advance Your Career with a Growing Company</h1>
+            <h1>Careers</h1>
             
             <p>
                 We're experiencing dynamic growth. As a result, we're looking for smart, ambitious, team-oriented professionals to help us meet increasing demand for our agitation agglomeration equipment, services and systems. This equipment is used in the production of spherical pellets in a wide range of applications and materials.
