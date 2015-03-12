4ef26d6224de660e8ef2dfcaac9a5410819b1bf2
Levy-mini <levy-mini@Levy-minis-Mac-mini.local>
2015-03-12 13:12:51 -0400

    Addition of the careers page

---
+++ careers.php
@@ -0,0 +1,31 @@
+<?php include_once('includes/header.php'); ?>
+    
+    <div id="banner">
+    </div><!--banner-->
+    
+    <div class="content">
+    	<div class="col-left">
+        	
+            <h1>Advance Your Career with a Growing Company</h1>
+            
+            <p>
+                We're experiencing dynamic growth. As a result, we're looking for smart, ambitious, team-oriented professionals to help us meet increasing demand for our agitation agglomeration equipment, services and systems. This equipment is used in the production of spherical pellets in a wide range of applications and materials.
+            </p>
+ 
+            <p>
+                If you have a technical or laboratory background and believe that one of our positions listed below may be right for you, <a href="mailto:info@marsmineral.com">send us your resume</a></p>
+            </p>
+            
+        </div><!--col-left-->
+        
+        <div class="col-right">
+        	<div style="margin-top:45px;">
+                    <?php include_once('includes/buttons.php'); ?>
+                </div>
+        </div><!--col-right-->
+    </div>
+    
+    <div class="clearfix"></div>
+    </div><!--content-->
+    
+<?php include_once('includes/footer.php'); ?>
